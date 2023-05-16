1d0189d7d11cce3a662021cb0fe5c6953d0977f1
WildcatGeoff <130526370+WildcatGeoff@users.noreply.github.com>
2023-05-15 20:40:48 -0700

    Added Input class for handling input from mouse and keyboard

---
+++ core/src/com/mygdx/game/Input.java
@@ -0,0 +1,37 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.InputProcessor;
+
+public class Input implements InputProcessor{
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		return false;
+	}
+
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		return false;
+	}
+
+	public boolean touchDragged(int x, int y, int pointer) {
+		return false;
+	}
+
+	public boolean mouseMoved(int x, int y) {
+		return false;
+	}
+
+	public boolean scrolled(float amountX, float amountY) {
+		return false;
+	}
+}
