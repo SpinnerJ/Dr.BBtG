41548d35d9048d4d11e99280e0dbeb0959ad3d90
WildcatGeoff <130526370+WildcatGeoff@users.noreply.github.com>
2023-05-30 23:19:25 -0700

    Added gameStates and FSM like system to switch between menus and play states

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -23,6 +23,8 @@ public class Game extends ApplicationAdapter{
 	private Database db;
 	private ArrayList<Mob> enemies = new ArrayList<Mob>();
 	private ArrayList<Texture> graphics = new ArrayList<Texture>();
+	private enum gameStates{MENU1,MENU2,START,PLAYING,HIGHSCORE};
+	private gameStates currentState = gameStates.MENU1;
 	
 	@Override
 	public void create () {
@@ -36,7 +38,7 @@ public class Game extends ApplicationAdapter{
 		test = new Mob(49,49,6,6,graphics.get(1),10,0); //mob given to player
 		player = new Player(test); //player object
 		m = player.getMob(); //reference to player's mob
-		Gdx.input.setInputProcessor(new Input(player)); //links libGDX input to our input class
+		Gdx.input.setInputProcessor(new Input(player,camera)); //links libGDX input to our input class
 		db = new Database();
 		db.connect();
 		String test = db.queryAll();
@@ -53,82 +55,116 @@ public class Game extends ApplicationAdapter{
 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //use openGL to clear the screen in hardware
 		
-		//if player is pressing rotating key, rotate the player
-		if(player.getRotating() != 0)
+		switch(currentState)
 		{
-			m.setAngle(m.getAngle()+(player.getRotating()*5));
+		case MENU1:
+			//initial menu on loading the game
+			if(player.getAccelerating() != 0)
+			{
+				currentState = gameStates.PLAYING;
+			}
+			batch.begin(); //start drawing graphics
+			batch.draw(background,0,0,camera.viewportWidth,camera.viewportHeight); //draw camera viewport
+			batch.end();
+			//Gdx.app.exit();
+			break;
+		case MENU2:
+			//menu when pressing escape while playing a game
+			batch.begin(); //start drawing graphics
+			batch.draw(background,0,0,camera.viewportWidth,camera.viewportHeight); //draw camera viewport
+			batch.end();
+			break;
+		case START:
+			//reset state of game so that health is maxed, time is zero, etc
+			//then go straight into playing state
+			batch.begin(); //start drawing graphics
+			batch.draw(background,0,0,camera.viewportWidth,camera.viewportHeight); //draw camera viewport
+			batch.end();
+			break;
+		case PLAYING:
+			//if player is pressing rotating key, rotate the player
+			if(player.getRotating() != 0)
+			{
+				m.setAngle(m.getAngle()+(player.getRotating()*5));
+			}
+			
+			//accelerate player if acceleration key is pressed
+			if(player.getAccelerating() != 0)
+			{
+				player.setAcceleration((player.getAcceleration()+(player.getAccelerating()*0.0001f)));
+				player.setAceAngle(m.getAngle());
+			}
+			
+			//set the speed of the player based on acceleration
+			m.setSpeed(m.getSpeed()+player.getAcceleration()*1f);
+			if(m.getSpeed()<0)
+			{
+				m.setSpeed(0);
+			}
+			
+			//keep player to max speed
+			if(m.getSpeed() > player.getMaxSpeed())
+			{
+				m.setSpeed(player.getMaxSpeed());
+			}
+			else if(m.getSpeed() < (player.getMaxSpeed()*-1))
+			{
+				m.setSpeed(player.getMaxSpeed()*-1);
+			}
+			//get how fast the player is moving in the x direction
+			//+90 is to compensate for sprite images pointing up instead of to the right
+			//ace angle is the angle the ship is pointing at when accelerating, this allows to be able to move in
+			//a direction while still being able to rotate freely
+			float speedX = (float)Math.cos(Math.toRadians(player.getAceAngle()+90));
+			speedX = speedX*m.getSpeed();
+			
+			//get how fast the player is moving in the y direction
+			float speedY = (float)Math.sin(Math.toRadians(player.getAceAngle()+90));
+			speedY = speedY*m.getSpeed();
+			
+			//update the position of the player
+			m.setX(speedX+m.getX());
+			m.setY(speedY+m.getY());
+			
+			//check if player is at a boundary, move to opposite side of screen if true
+			if(m.getX()<0)
+			{
+				m.setX(100);
+			}
+			if(m.getY()<0)
+			{
+				m.setY(100);
+			}
+			if(m.getX()>100)
+			{
+				m.setX(0);
+			}
+			if(m.getY()>100)
+			{
+				m.setY(0);
+			}
+			
+			batch.begin(); //start drawing graphics
+			batch.draw(background,0,0,camera.viewportWidth,camera.viewportHeight); //draw camera viewport
+			for(int i=0;i<enemies.size();i++)
+			{
+				enemies.get(i).draw(batch);
+			}
+			test.draw(batch);//draw player's mob
+			batch.end();
+			break;
+		case HIGHSCORE:
+			break;
 		}
-		
-		//accelerate player if acceleration key is pressed
-		if(player.getAccelerating() != 0)
-		{
-			player.setAcceleration((player.getAcceleration()+(player.getAccelerating()*0.0001f)));
-			player.setAceAngle(m.getAngle());
-		}
-		
-		//set the speed of the player based on acceleration
-		m.setSpeed(m.getSpeed()+player.getAcceleration()*1f);
-		if(m.getSpeed()<0)
-		{
-			m.setSpeed(0);
-		}
-		
-		//keep player to max speed
-		if(m.getSpeed() > player.getMaxSpeed())
-		{
-			m.setSpeed(player.getMaxSpeed());
-		}
-		else if(m.getSpeed() < (player.getMaxSpeed()*-1))
-		{
-			m.setSpeed(player.getMaxSpeed()*-1);
-		}
-		//get how fast the player is moving in the x direction
-		//+90 is to compensate for sprite images pointing up instead of to the right
-		//ace angle is the angle the ship is pointing at when accelerating, this allows to be able to move in
-		//a direction while still being able to rotate freely
-		float speedX = (float)Math.cos(Math.toRadians(player.getAceAngle()+90));
-		speedX = speedX*m.getSpeed();
-		
-		//get how fast the player is moving in the y direction
-		float speedY = (float)Math.sin(Math.toRadians(player.getAceAngle()+90));
-		speedY = speedY*m.getSpeed();
-		
-		//update the position of the player
-		m.setX(speedX+m.getX());
-		m.setY(speedY+m.getY());
-		
-		//check if player is at a boundary, move to opposite side of screen if true
-		if(m.getX()<0)
-		{
-			m.setX(100);
-		}
-		if(m.getY()<0)
-		{
-			m.setY(100);
-		}
-		if(m.getX()>100)
-		{
-			m.setX(0);
-		}
-		if(m.getY()>100)
-		{
-			m.setY(0);
-		}
-		
-		batch.begin(); //start drawing graphics
-		batch.draw(background,0,0,camera.viewportWidth,camera.viewportHeight); //draw camera viewport
-		for(int i=0;i<enemies.size();i++)
-		{
-			enemies.get(i).draw(batch);
-		}
-		test.draw(batch);//draw player's mob
-		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		background.dispose();
-		test.dispose();
+		//cleaning up memory by getting rid of textures
+		for(int i = 0;i<graphics.size();i++)
+		{
+			graphics.get(i).dispose();
+		}
 		batch.dispose();
 	}
 	
@@ -181,5 +217,8 @@ public class Game extends ApplicationAdapter{
 		graphics.add(new Texture(Gdx.files.internal("background.png")));
 		graphics.add(new Texture(Gdx.files.internal("playerShip.png")));
 		graphics.add(new Texture(Gdx.files.internal("enemy1.png")));
+		graphics.add(new Texture(Gdx.files.internal("enemy1.png")));
+		graphics.add(new Texture(Gdx.files.internal("enemy1.png")));
+		graphics.add(new Texture(Gdx.files.internal("buttonBlue.png")));
 	}
 }
